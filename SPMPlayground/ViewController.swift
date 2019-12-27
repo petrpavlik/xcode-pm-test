5fcabaf627dcbf4be5c4fd815fa33b5b8d1c01d2
Petr Pavlik <petrpavlik@me.com>
2019-12-27 15:14:36 +0100

    Initial Commit

---
+++ SPMPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPMPlayground
+//
+//  Created by Petr Pavlik on 27/12/2019.
+//  Copyright © 2019 Petr Pavlik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
